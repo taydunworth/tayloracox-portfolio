3253d4dc2fd6a4acc826e174d3224cf3300100e0
Taylor Cox <taylorashtoncox@gmail.com>
2018-07-05 19:15:32 -0400

    Creates work section with js - Adds array of objects to house data for work pieces - Imports array into main js file and loops through data

--- source/index.js
+++ source/index.js
@@ -1,5 +1,32 @@
-import './style/style.sass';
-import './style/style.css';
+import "./style/style.sass";
+import "./style/style.css";
+import * as work from "./work.js";
+
+const allWork = work.workItems;
+
+// Work
+for (let i = 0; i < allWork.length; i++) {
+  const workList = document.querySelector(".work-list");
+  const workListItem = document.createElement("li");
+  const workDiv = document.createElement("div");
+  const workTitle = document.createElement("h4");
+  const workImage = document.createElement("img");
+  const workButton = document.createElement("a");
+
+  workTitle.innerHTML = allWork[i].title;
+  workButton.innerHTML = "Learn More";
+  workButton.href = allWork[i].url;
+
+  workDiv.style.backgroundImage = `url(${allWork[i].img})`;
+
+  workDiv.appendChild(workTitle);
+  workDiv.appendChild(workImage);
+  workDiv.appendChild(workButton);
+  workListItem.appendChild(workDiv);
+  workList.appendChild(workListItem);
+}
+
+// Accordion
 
 const acc = document.getElementsByClassName("accordion");
 
---
+++ source/work.js
@@ -0,0 +1,49 @@
+export const workItems = [
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  },
+
+  {
+    "title": "Lorem Ipsum",
+    "img": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    "url": "/lorem-ipsum"
+  }
+]
