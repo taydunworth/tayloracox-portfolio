bcd743560a7c75a4c4f4be909c919dbfb2f09782
Taylor Cox <taylorashtoncox@gmail.com>
2018-07-04 20:59:46 -0400

    Updates styling on home page - Adds accordions for Speaking and Involvement sections - Lays out the flexbox for Work section

--- source/index.js
+++ source/index.js
@@ -1,2 +1,16 @@
 import './style/style.sass';
 import './style/style.css';
+
+const acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
